60772880cad840964d8d91c18c6644448ded4b40
mytoken <149029169+la-rosilva@users.noreply.github.com>
2025-01-01 19:53:55 +0530

    Add files via upload

---
+++ Disaster_management/disaster_management/src/components/SafetyMeasures.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const SafetyMeasures = () => {
+  return (
+    <div className='container w-full h-screen'>
+        <h4 className='mx-3 my-5 text-center font-semibold text-2xl text-neutral-500'>Preparedness and Safety Measures for Natural Disasters</h4>
+        <div className='flex'>
+      <div className='w-1/2 space-y-2'>
+      <button className='text-xl bg-blue-50 p-3 rounded-lg mt-5 box-shadow'>Earthquake</button>
+      </div>
+      <div className='w-1/2 space-y-2'>
+      
+      </div>
+      </div>
+    </div>
+  )
+}
+
+export default SafetyMeasures
